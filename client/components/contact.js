e4f9d4e783a834b234d782b4ef5df75cb86f4af0
Katelynn Burns <katelynnburns655@gmail.com>
2021-01-15 14:15:45 -0500

    WIP: Starting point

---
+++ client/components/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {auth} from '../store'
+
+const Contact = props => {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  )
+}
+
+export default Contact
---
+++ client/components/socialMedia.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {auth} from '../store'
+
+const SocialMedia = props => {
+  const {name, displayName, handleSubmit, error} = props
+
+  return (
+    <div>
+      <div>Contact</div>
+      <form onSubmit={handleSubmit} name={name}>
+        <div>
+          <label htmlFor="email">
+            <small>Email</small>
+          </label>
+          <input name="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.response.data} </div>}
+      </form>
+      <a href="/auth/google">{displayName} with Google</a>
+    </div>
+  )
+}
+
+export default SocialMedia
