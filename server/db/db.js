01183c3d6670a32ec5eeb6e80c704b18d73e3697
Katelynn Burns <katelynnburns655@gmail.com>
2021-01-19 18:13:12 -0500

    Bug: connecting db

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,7 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name
 
 const db = new Sequelize(
-  `postgres://kbpeuptwajzxll:8313e3e5ca674bca61a74956aa185ccde9f07ecb9ab17892edffc13006f48593@ec2-34-192-72-159.compute-1.amazonaws.com:5432/da41fdbgi1s055` ||
-    `postgres://localhost:5432/${databaseName}`,
+  `postgres://kbpeuptwajzxll:8313e3e5ca674bca61a74956aa185ccde9f07ecb9ab17892edffc13006f48593@ec2-34-192-72-159.compute-1.amazonaws.com:5432/da41fdbgi1s055`,
   {
     logging: false
   }
