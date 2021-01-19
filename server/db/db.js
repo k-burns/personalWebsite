0930727c5034a1b8b0a32e4105af6d99aa652339
Katelynn Burns <katelynnburns655@gmail.com>
2021-01-19 18:41:46 -0500

    Bug: Still fixing db

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,7 @@ const pkg = require('../../package.json')
 const databaseName = pkg.name
 
 const db = new Sequelize(
-  `postgres://kbpeuptwajzxll:8313e3e5ca674bca61a74956aa185ccde9f07ecb9ab17892edffc13006f48593@ec2-34-192-72-159.compute-1.amazonaws.com:5432/da41fdbgi1s055`,
+  `postgres://kbpeuptwajzxll:8313e3e5ca674bca61a74956aa185ccde9f07ecb9ab17892edffc13006f48593@ec2-34-192-72-159.compute-1.amazonaws.com:5432/da41fdbgi1s055?sslmode=require`,
   {
     logging: false
   }
