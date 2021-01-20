4dfc25cb7ad6399a8dba99f14c4495e4d89f5bca
Katelynn Burns <katelynnburns655@gmail.com>
2021-01-20 15:30:30 -0500

    Update: Message about mobile

--- client/components/main.js
+++ client/components/main.js
@@ -9,6 +9,10 @@ const Main = props => {
         Welcome! This site is a work in progress. I am constantly making
         updates, so please come back often. Thank you for coming!
       </div>
+      <div>
+        This webpage is currently in not designed for mobile. A more mobile
+        version will be released in the future.
+      </div>
       <img src="https://i2-prod.manchestereveningnews.co.uk/incoming/article9313047.ece/ALTERNATES/s615/JS64312931.jpg" />
     </div>
   )
--- public/bundle.js
+++ public/bundle.js
@@ -194,7 +194,7 @@ var Main = function Main(props) {
       displayName = props.displayName,
       handleSubmit = props.handleSubmit,
       error = props.error;
-  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Welcome! This site is a work in progress. I am constantly making updates, so please come back often. Thank you for coming!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "Welcome! This site is a work in progress. I am constantly making updates, so please come back often. Thank you for coming!"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, "This webpage is currently in not designed for mobile. A more mobile version will be released in the future."), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
     src: "https://i2-prod.manchestereveningnews.co.uk/incoming/article9313047.ece/ALTERNATES/s615/JS64312931.jpg"
   }));
 };
