9863be9da95c969c1de61a8642292abe9459fd3d
Katelynn Burns <katelynnburns@Katelynns-Air.home>
2021-10-28 19:43:16 -0400

    Feature: Contact form

--- client/components/contact.js
+++ client/components/contact.js
@@ -1,16 +1,70 @@
 import React from 'react'
+import {Form, Input, TextArea, Button} from 'semantic-ui-react'
+import emailjs from 'emailjs-com'
+import Swal from 'sweetalert2'
+
+const userId = 'user_lQpVcq9iHhvOposOKJnmO'
+const serviceId = 'service_ivyebii'
+const templateId = 'template_pwgt98q'
 
 const Contact = props => {
+  const handleSubmit = e => {
+    e.preventDefault()
+    emailjs.sendForm(serviceId, templateId, e.target, userId).then(
+      result => {
+        console.log(result.text)
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent! Thank You!'
+        })
+      },
+      error => {
+        console.log(error.text)
+        Swal.fire({
+          icon: 'error',
+          title: 'Error Sending Message:',
+          text: error.text
+        })
+      }
+    )
+    e.target.reset()
+  }
   return (
     <div>
-      <h1>📇 Contact</h1>
-      <div className="contact-body">
-        <h2>📧 Email:</h2>
-        <a href="mailto: katelynnburns655@gmail.com">
-          katelynnburns655@gmail.com
-        </a>
-        <h2>📱 Phone:</h2>
-        <a href="tel:+15413981208">541-398-1208</a>
+      <h1>📇 Contact Me:</h1>
+      <h3>Please Reach Out Using the Form Below. I Will Respond ASAP</h3>
+      <div className="contact-form">
+        <Form onSubmit={handleSubmit}>
+          <Form.Field
+            id="email-entry"
+            control={Input}
+            label="E-mail"
+            name="user_email"
+            placeholder="Email"
+            required
+            icon="mail"
+            iconPosition="left"
+          />
+          <Form.Field
+            id="name-entry"
+            control={Input}
+            label="Name"
+            name="user_name"
+            placeholder="Name"
+            required
+            icon="user circle"
+            iconPosition="left"
+          />
+          <Form.Field
+            id="message-entry"
+            control={TextArea}
+            label="Message"
+            name="user_message"
+            placeholder="Reason for Reaching Out"
+            required
+          />
+          <Button type="submit">Submit</Button>
+        </Form>
       </div>
     </div>
   )
--- client/components/socialMedia.js
+++ client/components/socialMedia.js
@@ -5,26 +5,41 @@ const SocialMedia = props => {
   return (
     <div>
       <h1>Social Media</h1>
+      <div className="social-container">
+        <div className="social-item">
+          <a
+            href="https://www.linkedin.com/in/katelynn-burns/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/LI-In-Bug.png" />
+          </a>
+        </div>
+        <div className="social-item">
+          <a
+            href="https://twitter.com/Katelyn41648210"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/2021-Twitter-logo-blue.png" />
+          </a>
+        </div>
+        <div className="social-item">
+          <a
+            href="https://katelynnburns655.medium.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="/Medium-Symbol-Black-RGB@1x.jpg" />
+          </a>
+        </div>
+      </div>
       <div>
         <TwitterTimelineEmbed
           sourceType="profile"
           screenName="Katelyn41648210"
           options={{height: 300}}
         />
-      </div>
-      <div>
-        <a
-          href="https://www.linkedin.com/in/katelynn-burns/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src="../../public/LI-In-Bug.png" />
-        </a>
-      </div>
-      <div>
-        <a href="https://katelynnburns655.medium.com/" target="_blank">
-          Medium
-        </a>
       </div>
     </div>
   )
